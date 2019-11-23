3bf5a244732d89eae87f16dd92f3361ee27de4b3
CmdrKittens <52665651+CmdrKittens@users.noreply.github.com>
2019-11-23 17:48:34 -0500

    Add in docs / fix todo

--- src/main/java/com/massivecraft/factions/cmd/CmdStatus.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdStatus.java
@@ -28,12 +28,12 @@ public class CmdStatus extends FCommand {
         for (FPlayer fp : context.faction.getFPlayers()) {
             String humanized = DurationFormatUtils.formatDurationWords(System.currentTimeMillis() - fp.getLastLoginTime(), true, true) + TL.COMMAND_STATUS_AGOSUFFIX;
             String last = fp.isOnline() ? ChatColor.GREEN + TL.COMMAND_STATUS_ONLINE.toString() : (System.currentTimeMillis() - fp.getLastLoginTime() < 432000000 ? ChatColor.YELLOW + humanized : ChatColor.RED + humanized);
-            // TODO DTR
             String power = "";
             if (FactionsPlugin.getInstance().getLandRaidControl() instanceof PowerControl) {
                 power = ChatColor.YELLOW + String.valueOf(fp.getPowerRounded()) + " / " + fp.getPowerMaxRounded() + ChatColor.RESET;
+            } else {
+                power = "n/a";
             }
-            // TODO new TL for DTR
             ret.add(String.format(TL.COMMAND_STATUS_FORMAT.toString(), ChatColor.GOLD + fp.getRole().getPrefix() + fp.getName() + ChatColor.RESET, power, last).trim());
         }
         context.fPlayer.sendMessage(ret);
