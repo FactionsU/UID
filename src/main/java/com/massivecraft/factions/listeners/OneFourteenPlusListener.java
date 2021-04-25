808fd3420223bcb0e4cd29c0ec725533303653c9
mbax <matt@phozop.net>
2021-04-25 18:05:28 -0400

    Allow lectern reading via config

---
+++ src/main/java/com/massivecraft/factions/listeners/OneFourteenPlusListener.java
@@ -0,0 +1,45 @@
+package com.massivecraft.factions.listeners;
+
+import com.massivecraft.factions.Board;
+import com.massivecraft.factions.FLocation;
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.FactionsPlugin;
+import com.massivecraft.factions.perms.PermissibleAction;
+import com.massivecraft.factions.util.TL;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerTakeLecternBookEvent;
+
+public class OneFourteenPlusListener extends AbstractListener {
+    private final FactionsPlugin plugin;
+
+    public OneFourteenPlusListener(FactionsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void doYouHaveALibraryCard(PlayerTakeLecternBookEvent event) {
+        Player player = event.getPlayer();
+        if (this.plugin.conf().factions().protection().getPlayersWhoBypassAllProtection().contains(player.getName())) {
+            return;
+        }
+
+        FPlayer me = FPlayers.getInstance().getByPlayer(player);
+        if (me.isAdminBypassing()) {
+            return;
+        }
+
+        Faction otherFaction = Board.getInstance().getFactionAt(new FLocation(event.getLectern().getLocation()));
+        if (this.plugin.getLandRaidControl().isRaidable(otherFaction)) {
+            return;
+        }
+
+        PermissibleAction action = PermissibleAction.CONTAINER;
+        if (!otherFaction.hasAccess(me, action)) {
+            me.msg(TL.GENERIC_NOPERMISSION, action);
+            event.setCancelled(true);
+        }
+    }
+}
