1b4520a7de1440d74a8d38d2653c7166da27f18c
mbax <matt@phozop.net>
2021-07-06 12:36:25 -0400

    Correctly send invite confirmation when inviting someone who is offline

--- src/main/java/com/massivecraft/factions/cmd/CmdInvite.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdInvite.java
@@ -32,7 +32,7 @@ public class CmdInvite extends FCommand {
 
         if (target.getFaction() == context.faction) {
             context.msg(TL.COMMAND_INVITE_ALREADYMEMBER, target.getName(), context.faction.getTag());
-            context.msg(TL.GENERIC_YOUMAYWANT.toString() + FCmdRoot.getInstance().cmdKick.getUsageTemplate(context));
+            context.msg(TL.GENERIC_YOUMAYWANT + FCmdRoot.getInstance().cmdKick.getUsageTemplate(context));
             return;
         }
 
@@ -47,22 +47,20 @@ public class CmdInvite extends FCommand {
         }
 
         context.faction.invite(target);
-        if (!target.isOnline()) {
-            return;
+        if (target.isOnline()) {
+            // Tooltips, colors, and commands only apply to the string immediately before it.
+            FancyMessage message = new FancyMessage(context.fPlayer.describeTo(target, true))
+                    .tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
+                    .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag())
+                    .then(TL.COMMAND_INVITE_INVITEDYOU.toString())
+                    .color(ChatColor.YELLOW)
+                    .tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
+                    .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag())
+                    .then(context.faction.describeTo(target)).tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
+                    .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag());
+
+            message.send(target.getPlayer());
         }
-
-        // Tooltips, colors, and commands only apply to the string immediately before it.
-        FancyMessage message = new FancyMessage(context.fPlayer.describeTo(target, true))
-                .tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
-                .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag())
-                .then(TL.COMMAND_INVITE_INVITEDYOU.toString())
-                .color(ChatColor.YELLOW)
-                .tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
-                .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag())
-                .then(context.faction.describeTo(target)).tooltip(TL.COMMAND_INVITE_CLICKTOJOIN.toString())
-                .command("/" + FactionsPlugin.getInstance().conf().getCommandBase().get(0) + " join " + context.faction.getTag());
-
-        message.send(target.getPlayer());
 
         //you.msg("%s<i> invited you to %s",context.fPlayer.describeTo(you, true), context.faction.describeTo(you));
         context.faction.msg(TL.COMMAND_INVITE_INVITED, context.fPlayer.describeTo(context.faction, true), target.describeTo(context.faction));
