91cd3f07f2b09ba305ed95fc90dd33d5066b7c6c
mbax <matt@phozop.net>
2026-02-08 14:40:46 -0500

    Better audience command behavior for Paper servers

--- bukkit/src/main/java/dev/kitteh/factions/command/LilAudience.java
+++ bukkit/src/main/java/dev/kitteh/factions/command/LilAudience.java
@@ -3,14 +3,16 @@ package dev.kitteh.factions.command;
 import dev.kitteh.factions.util.ComponentDispatcher;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
 
 @ApiStatus.Internal
 @NullMarked
-public record LilAudience(Sender sender) implements Audience {
+// Lil Audience is my (w)rapper name
+public record LilAudience(CommandSender sender) implements Audience {
     @Override
     public void sendMessage(Component message) {
-        ComponentDispatcher.send(sender.sender(), message);
+        ComponentDispatcher.send(sender, message);
     }
 }
